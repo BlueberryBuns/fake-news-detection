e108cbe42b1b70fa363d772aecedf61eda65427f
BlueberryBuns <hulewicz.k@gmail.com>
2023-05-25 16:47:55 +0200

    Add initial setup with no data

--- streaming_approach/generate_embeddings.py
+++ streaming_approach/generate_embeddings.py
@@ -12,11 +12,11 @@ device = torch.device("mps")
 test = 1
 languages = "english_german"
 checkpoints = [
+    "sentence-transformers/all-MiniLM-L6-v2",
     "sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2",
     "bert-base-multilingual-cased",
     "distilbert-base-cased",
     "bert-base-cased",
-    "sentence-transformers/all-MiniLM-L6-v2",
 ]
 
 def divide_chunks(list_of_sentences, elements):
@@ -29,24 +29,37 @@ for checkpoint in checkpoints:
     model.to(device)
     model.eval()
 
-    full_data = pd.read_csv(f"../data/streams/{languages}_combined.csv", index_col=0)
-    text_data = full_data["text"].tolist()
+    for reduced in ["", "reduced_"]:
+        if not reduced:
+            path_ = "../data/streams/english_german_combined_REDUCED.csv"
+        else:
+            path_ = "../data/static/english/reduced.csv"
 
-    for batch_index, sentences in tqdm(enumerate(divide_chunks(text_data, 100))):
-        batch = tokenizer(sentences, padding=True, truncation=True, max_length=512, return_tensors="pt")
-        batch.to(device)
+        full_data = pd.read_csv(path_).iloc[:200]
+        text_data = full_data["text"].tolist()
 
-        with torch.no_grad():
-            encoded_layers = model(**batch, return_dict=True, output_hidden_states=True)
-            sentence_embeddings: torch.Tensor = torch.mean(encoded_layers[0], axis=1)
-            torch.save(sentence_embeddings, f"batch_{batch_index}.pt")
-    filenames = glob.glob("batch_*.pt")
-    print(filenames)
-    combined = torch.load(filenames[0])
-    for filename in filenames[1:]:
-        additional = torch.load(filename)
-        combined = torch.cat([combined, additional])
-    torch.save(combined, f"embeddings_{checkpoint.replace('-', '_').replace('/', '__')}_mean_0.pt")
 
-    for filename in filenames:
-        os.remove(filename)
+
+        for batch_index, sentences in tqdm(enumerate(divide_chunks(text_data, 50))):
+            # batch = tokenizer(sentences, padding=True, truncation=True, max_length=512, return_tensors="pt")
+            batch = tokenizer(sentences, padding=True, truncation=True, max_length=512, return_tensors="pt")
+            # import ipdb; ipdb.set_trace()
+            batch.to(device)
+
+            with torch.no_grad():
+                encoded_layers = model(**batch, return_dict=True, output_hidden_states=True)
+                sentence_embeddings: torch.Tensor = torch.mean(encoded_layers[0], axis=1)
+                torch.save(sentence_embeddings, f"{reduced}batch_{batch_index}.pt")
+
+        filenames = [f"{reduced}batch_{index}.pt" for index in range(batch_index + 1)]
+        print(filenames)
+        combined = torch.load(filenames[0])
+        for filename in filenames[1:]:
+            additional = torch.load(filename)
+            combined = torch.cat([combined, additional])
+            del additional
+        torch.save(combined, f"data/{(lambda x: 'static' if x else 'streaming')(reduced)}_embeddings_{checkpoint.replace('-', '_').replace('/', '__')}_mean_0.pt")
+
+        for filename in filenames:
+            os.remove(filename)
+    del model
